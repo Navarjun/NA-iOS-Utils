22e29589565c112f7172e22f05924cf9f188a1ff
Navarjun <navarjunsingh008@gmail.com>
2016-01-20 18:09:22 -0500

    - Solving AnimatedButton rotation scaling.

--- AnimatedButton.swift
+++ AnimatedButton.swift
@@ -6,6 +6,7 @@
 // Button animates as follows:
 // when user taps it, it shrinks
 // when user leaves it, it grows from shrunk state and then goes back to actual size(scale = 1)
+// This class uses UIView-ext.swift
 //
 // Customizable properties (though this is a obj-c concept, but I am going to use it in comments anyway)
 // @property animationTime - time that button takes to grow or shrink
@@ -19,12 +20,11 @@ class AnimatedButton: UIButton {
     
     private var context : UnsafeMutablePointer<Void>  = UnsafeMutablePointer()
     private var lastState = UIControlState.Normal
-    private var view = UIView()
     
-    var animationTime = 0.1
-    var tappedScale = 0.9
-    var untappedScale = 1.05
-    var tappedAlphaDelta : CGFloat  = CGFloat(0.1)
+    @IBInspectable var animationTime = 0.1
+    @IBInspectable var tappedScale: CGFloat = 0.9
+    @IBInspectable var untappedScale: CGFloat = 1.05
+    @IBInspectable var tappedAlphaDelta : CGFloat  = CGFloat(0.1)
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -43,18 +43,19 @@ class AnimatedButton: UIButton {
             case UIControlState.Normal:
                 layer.removeAllAnimations()
                 UIView.animateWithDuration(animationTime, animations: {
-                    self.layer.transform = CATransform3DMakeScale(CGFloat(self.untappedScale), CGFloat(self.untappedScale), 1)
+                    self.layer.transform = self.newScaleTransform(self.untappedScale)
+                    
                     self.alpha = self.alpha + self.tappedAlphaDelta
                     }, completion: { (finished) in
                         UIView.animateWithDuration(self.animationTime, animations: {
-                            self.layer.transform = CATransform3DMakeScale(1, 1, 1)
+                            self.layer.transform = self.newScaleTransform(1)
                         })
                 })
             case UIControlState.Highlighted:
                 layer.removeAllAnimations()
                 UIView.animateWithDuration(animationTime, animations: {
                     self.alpha = self.alpha - self.tappedAlphaDelta
-                    self.layer.transform = CATransform3DMakeScale(CGFloat(self.tappedScale), CGFloat(self.tappedScale), 1)
+                    self.layer.transform = self.newScaleTransform(self.tappedScale)
                 })
             default:
                 break
@@ -67,6 +68,12 @@ class AnimatedButton: UIButton {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(animationTime*2*Double(NSEC_PER_SEC) + 0.1)), dispatch_get_main_queue(), {
             self.sendActionsForControlEvents(UIControlEvents.TouchUpInside)
         })
+    }
+    
+    private func newScaleTransform(scale: CGFloat) -> CATransform3D {
+        let scaleTransform = CATransform3DMakeScale(scale, scale, 1)
+        let rotationTransform = CATransform3DMakeRotation(self.rotationInDegrees, 0, 0, 1)
+        return CATransform3DConcat(scaleTransform, rotationTransform)
     }
     
     deinit {
